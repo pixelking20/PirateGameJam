5eda1aacdc98b281e777dd62c87501b5bbcfac19
Matthew Ramsey <36775051+pixelking20@users.noreply.github.com>
2024-07-21 14:14:55 -0400

    Imported TMP and set up a basic start scene

---
+++ Assets/UI/EventSystem/EventTriggers/BasicNamedButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CustomUIEvents.Triggers;
+
+public class BasicNamedButton : BaseUITrigger
+{
+    public void Clicked()
+    {
+        Debug.Log("Testing Unity On Click");
+    }
+}
